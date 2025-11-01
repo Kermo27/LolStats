7c6250e928d4e58f79223e4a3ba03f0984fdd9c4
Kacper Juszkiewicz <kacper.juszkiewicz7@gmail.com>
2025-11-02 00:02:08 +0100

    replace bootstrap with tailwindcss

--- Models/MatchEntry.cs
+++ Models/MatchEntry.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace LolStatsTracker.Models;
 
 public class MatchEntry
@@ -5,9 +7,13 @@ public class MatchEntry
     public Guid Id { get; set; }
     public DateTime Date { get; set; } = DateTime.Now;
     public bool Win { get; set; }
+    [Required(ErrorMessage = "Choose champion")]
     public string Champion { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Choose support")]
     public string Support { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Choose enemy bot")]
     public string EnemyBot { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Choose enemy support")]
     public string EnemySupport { get; set; } = string.Empty;
     public int Kills { get; set; }
     public int Deaths { get; set; }
